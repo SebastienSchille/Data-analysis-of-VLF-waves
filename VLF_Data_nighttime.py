3a0c5d6cdd4cf360d785d8ebc7863d988dc4e29a
Sebastien Schille <sebastien.j.schille@gmail.com>
2021-04-06 18:39:57 +0100

    Nighttime program modifications

--- VLF_Data_nighttime.py
+++ VLF_Data_nighttime.py
@@ -1,23 +1,23 @@
 import numpy as np
 
-
+#Function to split the time into hours and minutes
 def read_int(char):
     return float(char.strip(b'"').replace(b':',b'.'))
 
-
+#Read the csv file and save into 'hour' and 'mins' varriables
 nighttime = np.genfromtxt('Nighttime data 2004.csv', dtype=float, usecols=(2,3), delimiter=',', converters={a: read_int for a in range(366)})
 hour = np.floor(nighttime)
-mins = (nighttime - hour) * 100
-
+mins = np.round((nighttime - hour) * 100)
+#Convert to a 20s time interval formate
 data_point1  = hour * 180
 data_point2 = mins * 3
-
-daylight_start = data_point1[:,0] + data_point2[:,0]
-daylight_end = data_point1[:,1] + data_point2[:,1]
-
+#Save the daylight satrt and end times into a lidt
+daylight_start = np.array(data_point1[:,0] + data_point2[:,0])
+daylight_end = np.array(data_point1[:,1] + data_point2[:,1])
+#Zero end values that go beyond mightnight
 for i in range(len(daylight_end)):
     if daylight_end[i] < 300:
         daylight_end[i] = daylight_end[i]*0
-
-#print(daylight_start)
-#print(daylight_end)
+daylight_times = np.column_stack((daylight_start, daylight_end))
+#np.savetxt('vlf_data_output.txt',daylight_times, delimiter=',', fmt='%4i')
+#print(daylight_times)
